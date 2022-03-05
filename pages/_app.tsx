08165dfef9ee53d3fba8787bdd6943436416d165
Everton Andrade <evertonfa7@gmail.com>
2022-03-05 01:42:43 -0300

    improvement on home component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
+import AppContainer from '../components/app-container';
+import Navigation from '../components/navigation';
+import Footer from '../components/footer';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider attribute="class" enableSystem={false}>
+        <Head>
+          <title>Everton Andrade | Personal Website</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <AppContainer>
+          <Navigation />
+          <Component {...pageProps} />
+          <Footer />
+        </AppContainer>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,29 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import styles from '../styles/home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Everton Andrade | Personal Website</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main>
-        <h1>Everton Andrade</h1>
-        <p>Site em manutenção...</p>
+    <>
+      <main className={styles.mainContent}>
+        <section className={styles.aboutSection}>
+          <Image
+            src="https://avatars.githubusercontent.com/u/43795982?v=4"
+            alt="avatar"
+            width={80}
+            height={80}
+            className={styles.avatarImage}
+          />
+          <div>
+            <h2>Everton Andrade</h2>
+            <p>
+              Desenvolvedor web, estudante de ciência da computação e entusiasta
+              de software livre.
+            </p>
+          </div>
+        </section>
       </main>
-    </div>
+    </>
   );
 };
 
