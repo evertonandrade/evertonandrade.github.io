fd68930938b7399535bcacb36951183d13f56952
Everton Andrade <evertonfa7@gmail.com>
2022-04-15 11:51:40 -0300

    refactor nav links

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,23 +1,32 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/navigation.module.css';
 
+const navLinks = [
+  { title: 'home', href: '/' },
+  { title: 'blog', href: '/blog' },
+  { title: 'portfolio', href: '/portfolio' },
+  { title: 'resume', href: '/resume' },
+];
+
 const Navigation = () => {
+  const router = useRouter();
+
+  const linkStyle = (href: string) =>
+    `${styles.link} ${activeLink(href, router.asPath)}`;
+
+  const activeLink = (href: string, pathname: string) =>
+    href === pathname ? styles.active : '';
+
   return (
     <header className={styles.header}>
       <div className={styles.containerNav}>
         <nav className={styles.nav}>
-          <Link href="/">
-            <a className={styles.link}>home</a>
-          </Link>
-          <Link href="/blog">
-            <a className={styles.link}>blog</a>
-          </Link>
-          <Link href="/portfolio">
-            <a className={styles.link}>portfolio</a>
-          </Link>
-          <Link href="/resume">
-            <a className={styles.link}>resume</a>
-          </Link>
+          {navLinks.map((navLink) => (
+            <Link href={navLink.href} key={navLink.href}>
+              <a className={linkStyle(navLink.href)}>{navLink.title}</a>
+            </Link>
+          ))}
         </nav>
       </div>
     </header>
